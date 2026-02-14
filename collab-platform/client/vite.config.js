bd3b8766f002900febed1ac0dc5ca45ce430dd5c
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2026-02-14 21:52:31 +0530

    fix: update CORS for production

--- collab-platform/client/vite.config.js
+++ collab-platform/client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SkillSkirmish/',
   // --- ADD THIS SECTION ---
   // This tells Vite to replace any occurrence of 'global' with 'globalThis',
   // which is a standard variable available in both Node and browsers.
